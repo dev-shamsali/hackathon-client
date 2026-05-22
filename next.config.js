86320448cf8ef5165ad582d7b23e99d1f24a1981
dev-shams <dev.shamsali@gmail.com>
2026-05-22 16:54:57 +0530

    fix: update production backend URL to use HTTPS protocol

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const backendUrl = process.env.BACKEND_URL ||
   (process.env.NODE_ENV === 'production'
-    ? 'http://api-hackathon.nexcorealliance.com'
+    ? 'https://api-hackathon.nexcorealliance.com'
     : 'http://localhost:5000');
 
 const nextConfig = {
